07118289ff01defc12e0f4a98c146129cb78e6a1
enzoblain <blenzodu57@gmail.com>
2025-12-03 23:22:56 +0100

    fix: ci

--- benches/sha256_ref.rs
+++ benches/sha256_ref.rs
@@ -4,24 +4,19 @@ use std::hint::black_box;
 use std::time::Instant;
 
 pub fn bench_sha2_ref(c: &mut Criterion) {
-    let mut g = c.benchmark_group("sha256_ref");
-
-    g.bench_function("sha256_ref", |b| {
+    c.bench_function("sha256_ref", |b| {
         b.iter_custom(|iters| {
-            let data = [0u8; 64];
             let start = Instant::now();
 
             for _ in 0..iters {
                 let mut hasher = Sha256::new();
-                hasher.update(black_box(&data));
+                hasher.update(black_box(&black_box(&[0u8; 64])));
                 let _ = hasher.finalize();
             }
 
             start.elapsed()
         });
     });
-
-    g.finish();
 }
 
 criterion_group!(benches, bench_sha2_ref);
